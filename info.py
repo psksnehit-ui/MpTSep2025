f660c5b9a025e1fed6b2762c117bc1bcd47bf168
psksnehit-ui <psk.snehit@gmail.com>
2025-10-11 00:17:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -166,7 +166,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://moviesplus-mpt.koyeb.app/")
+URL = environ.get("URL", "https://telugu-wb-ser-mpt.koyeb.app/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
@@ -196,3 +196,4 @@ else:
 # Don't Remove Credit @VJ_Botz
 # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
 # Ask Doubt on telegram @KingVJ01
+
